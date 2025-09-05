31cb607a310bc88607c965e2c35ed075350f586c
Valter Sundström <valter.sundstrom@gmail.com>
2025-09-05 08:24:19 +0200

    remove old lipex

--- lipex/advanced/pattern_matching.ex
+++
@@ -1,7 +0,0 @@
-defmodule Lipex.Advanced.PatternMatching do
-  @moduledoc """
-  Handles pattern matching in Lipex syntax.
-  """
-  
-  # Stub module - functionality integrated into other modules
-end
